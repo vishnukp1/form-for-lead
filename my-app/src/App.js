cac23a1aa31cc23830b9f8caa87fe181700be80a
wishtony <vishnutoanother@gmail.com>
2023-11-22 18:37:17 +0530

    new commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -26,25 +26,25 @@ function App() {
       <Button variant="outline-danger">Cancel</Button></div>
      <div style={{paddingLeft:"22px"}}>
     
-<h6>Lead name</h6>
+<div className='div-text'>Lead name</div>
       <div className="inputWithIcon">
       <img className='icon-input' src={lead} alt=''></img>
         <input type="text" placeholder="Enter lead name" />
    
       </div>
-<h6>Email ID</h6>
+<div className='div-text'>Email ID</div>
       <div className="inputWithIcon">
       <img className='icon-input' src={iconeight} alt=''></img>
         <input type="text" placeholder="Example@anyemail.com" />
         
       </div>
-<h6>Phone number</h6>
+<div className='div-text'>Phone number</div>
       <div className="inputWithIcon">
       <img className='icon-input' src={iconseven} alt=''></img>
         <input type="text" placeholder="+91 8800688763" />
       
       </div>
-<h6>Address</h6>
+<div className='div-text'>Address</div>
       <div className="inputWithIcon inputIconBg">
       <img className='icon-input' src={iconone} alt=''></img>
         <input type="text" placeholder="Gugurgram ,India" />
@@ -53,39 +53,41 @@ function App() {
  <div className='img-div'>
   <img style={{width:"100%",height:"160px",marginBottom:"22px"}} src={iconfive} alt=''></img>
  </div>
+ 
+
    {/* <MapContainer /> */}
-<h6>Sale value</h6>
+<div className='div-text'>Sale value</div>
       <div className="inputWithIcon inputIconBg">
       <img className='icon-input' src={icontwo} alt=''></img>
         <input type="text" placeholder="50,00,000" />
        
       </div>
-<h6>Date</h6>
+<div className='div-text'>Date</div>
       <div className="inputWithIcon inputIconBg">
       <img className='icon-input' src={iconthree} alt=''></img>
         <input type="text" placeholder="29 August 2023" />
       
       </div>
-<h6>Time</h6>
+<div className='div-text'>Time</div>
      
       <div className="inputWithIcon inputIconBg">
       <img className='icon-input' src={times} alt=''></img>
         <input type="text" placeholder="20:21 pm" />
      
       </div>
-<h6>Options</h6>
+<div className='div-text'>Options</div>
       <div className="inputWithIcon inputIconBg">
       <img className='icon-input' src={iconfour} alt=''></img>
         <input type="text" placeholder="Email" />
        
       </div>
-      <h6>Products</h6>
+      <div className='div-text'>Products</div>
       <div className="inputWithIcon inputIconBg">
       <img className='icon-input' src={iconfour} alt=''></img>
         <input type="text" placeholder="Select option" />
       
       </div>
-      <h6>Notes</h6>
+      <div className='div-text'>Notes</div>
       <Form.Group controlId="exampleForm.ControlTextarea1">
     <Form.Control as="textarea" rows="4" placeholder="Enter note" />
   </Form.Group>
